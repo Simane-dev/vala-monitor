5630ef6878fac47597d7e65e60489591d96092c3
Simane-dev <simane.taif@gmail.com>
2026-07-25 05:00:26 +0100

    Implement BlacklistChecker class for domain checking

--- classes/BlacklistChecker.php
+++ classes/BlacklistChecker.php
@@ -1,1 +1,29 @@
+<?php
+require_once 'BaseChecker.php';
 
+class BlacklistChecker extends BaseChecker {
+    private $rbls = [
+        'zen.spamhaus.org',
+        'bl.spamcop.net'
+    ];
+
+    public function check($domain) {
+        $result = [
+            'status' => 'ok',
+            'blacklisted' => false,
+            'details' => []
+        ];
+
+        foreach ($this->rbls as $rbl) {
+            $check = $domain . '.' . $rbl;
+            if (checkdnsrr($check, 'A')) {
+                $result['blacklisted'] = true;
+                $result['status'] = 'listed';
+                $result['details'][] = $rbl;
+            }
+        }
+        
+        return $result;
+    }
+}
+?>
